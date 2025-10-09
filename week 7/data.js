b8c625f585541640772cc19ec177fc798dc96961
Pravallika-90073 <2420090073@klh.edu.in>
2025-10-09 10:45:06 +0530

    Add files via upload

---
+++ week 7/data.js
@@ -0,0 +1,8 @@
+export const books = [
+{ title: "Pride and Prejudice", author: "Jane Austen", price: 450, availability: "in stock" },
+{ title: "Atomic Habits", author: "James Clear", price: 399, availability: "out stock" },
+{ title: "Sapiens: A Brief History of Humankind", author: "Yuval Noah Harari", price: 599, availability: "in stock" },
+{ title: "Python for Everyone", author: "Ramya Krishna", price: 499, availability: "in stock" },
+{ title: "The Catcher in the Rye", author: "J.D. Salinger", price: 425, availability: "in stock" },
+{ title: "To Kill a Mockingbird", author: "Harper Lee", price: 350, availability: "out stock" },
+];
